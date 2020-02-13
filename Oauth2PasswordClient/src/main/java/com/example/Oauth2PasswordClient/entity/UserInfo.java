c23073d5fbeb18d96e024cfb8a56b50fbcad206c
minglongzhu <2661205047@qq.com>
2020-02-13 10:15:13 +0800

    init

---
+++ Oauth2PasswordClient/src/main/java/com/example/Oauth2PasswordClient/entity/UserInfo.java
@@ -0,0 +1,43 @@
+package com.example.Oauth2PasswordClient.entity;
+
+import java.util.List;
+
+public class UserInfo {
+
+    private List<String> roles;
+    private String introduction;
+    private String avatar;
+    private String name;
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+
+    public String getIntroduction() {
+        return introduction;
+    }
+
+    public void setIntroduction(String introduction) {
+        this.introduction = introduction;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
